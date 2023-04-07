8d69744b252abc39dadc675295c336979deb0fde
siva <siva636@gmail.com>
2023-04-07 14:59:57 +0530

    Authentication improved with error state

--- lib/src/auth/bloc/auth_bloc.dart
+++ lib/src/auth/bloc/auth_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:eat/src/auth/models/models.dart';
 import 'package:eat/src/auth/repository/auth_repository.dart';
 import 'package:eat/src/utils/enums/authentication_status.dart';
+import 'package:eat/src/utils/enums/view_status.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
@@ -32,8 +33,12 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     }
   }
 
-  _onSignOut(event, emit) {
-    authRepository.signOut();
-    emit(const AuthState.unauthenticated());
+  _onSignOut(event, emit) async {
+    try {
+      await authRepository.signOut();
+      emit(const AuthState.unauthenticated());
+    } catch (e) {
+      emit(const AuthState.error());
+    }
   }
 }
--- lib/src/auth/bloc/auth_state.dart
+++ lib/src/auth/bloc/auth_state.dart
@@ -2,14 +2,32 @@ part of 'auth_bloc.dart';
 
 @immutable
 class AuthState {
-  const AuthState._(this.authenticationStatus, [this.myUser]);
-  const AuthState.initial() : this._(AuthenticationStatus.unknown);
-  const AuthState.unknown() : this._(AuthenticationStatus.unknown);
-  const AuthState.unauthenticated()
-      : this._(AuthenticationStatus.unauthenticated);
-  const AuthState.authenticated(this.myUser)
-      : authenticationStatus = AuthenticationStatus.authenticated;
+  const AuthState._({
+    required this.viewStatus,
+    required this.authenticationStatus,
+    this.myUser,
+  });
+  const AuthState.initial()
+      : this._(
+          viewStatus: ViewStatus.initial,
+          authenticationStatus: AuthenticationStatus.unknown,
+        );
+  const AuthState.error()
+      : this._(
+          viewStatus: ViewStatus.failure,
+          authenticationStatus: AuthenticationStatus.unknown,
+        );
 
+  const AuthState.unauthenticated()
+      : this._(
+          viewStatus: ViewStatus.initial,
+          authenticationStatus: AuthenticationStatus.unauthenticated,
+        );
+  const AuthState.authenticated(this.myUser)
+      : viewStatus = ViewStatus.initial,
+        authenticationStatus = AuthenticationStatus.authenticated;
+
+  final ViewStatus viewStatus;
   final AuthenticationStatus authenticationStatus;
   final MyUser? myUser;
 }
